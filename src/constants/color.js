9bde4c623c6f56808d005afde72ada8464131fde
unknown <abhaysinghfzd2004@gmail.com>
2024-06-24 10:38:53 +0530

    commit

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,7 +1,7 @@
 export const orange = "#ea7020";
 export const orangeLight = "rgba(343, 112, 112,0.2)";
 
-export const grayColor = "rgba(247,247,247,100)";
+export const grayColor = "rgba(248,247,247,100)";
 export const lightBlue = "#294ab";
 export const matBlack = "#1c1c1c";
 export const bgGradient = "linear-gradient(rgb(223 225 109), rgb(249 159 159))";
